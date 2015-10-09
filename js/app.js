cb82debeb26d8d3cbcc9b6c348b0f44f299f8f81
Ramsey <rdarling42@gmail.com>
2015-10-09 15:48:23 -0400

    starting build

--- js/app.js
+++ js/app.js
@@ -1,3 +1,56 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
+
+$(document).ready(function(){
+    $('.show-my-shedule').click(function(){
+        $('.page').fadeOut();
+        $('.page.home').fadeIn();
+    });
+    $('.show-upcoming-programs').click(function(){
+        $('.page').fadeOut();
+        $('.page.upcoming-programs').fadeIn();
+    });
+    $('.button.tiny').click(function(){
+        $(this).toggleClass('success');
+    });
+});
+
+//define program objects
+
+var p1 = {
+    title: "ACM Student Research Competition",
+    day: "d27",
+    start: "3",
+    end: "5"
+};
+
+var p3 = {
+    title: "Appy Hour",
+    day: "d28",
+    start: "5",
+    end: "7"
+};
+
+var p5 = {
+    title: "Art Gallery",
+    day: "d24",
+    start: "9",
+    end: "7"
+};
+
+
+var p7 = {
+    title: "Art Gallery",
+    day: "d25",
+    start: "9",
+    end: "7"
+};
+
+
+var p9 = {
+    title: "Art Gallery",
+    day: "d26",
+    start: "9",
+    end: "7"
+};
