13a838a7432fd716f4d46eaf8b153464510f1e5f
Ramsey <rdarling42@gmail.com>
2015-10-23 10:09:53 -0400

    style tweaks, html structure change, working schedule time

--- js/app.js
+++ js/app.js
@@ -13,44 +13,95 @@ $(document).ready(function(){
     });
     $('.button.tiny').click(function(){
         $(this).toggleClass('success');
+        //here we mark the schedule as having been updated
+        var proid = $(this).attr('data-proid');
+        var pid = turn_proid_to_pid(proid);
+        set_time(pid);
     });
 });
 
 //define program objects
+var program_objects = [];
 
-var p1 = {
+program_objects[0] = {
     title: "ACM Student Research Competition",
     day: "d27",
     start: "3",
     end: "5"
 };
 
-var p3 = {
+program_objects[1] = {
     title: "Appy Hour",
     day: "d28",
     start: "5",
     end: "7"
 };
 
-var p5 = {
+program_objects[2] = {
     title: "Art Gallery",
     day: "d24",
     start: "9",
     end: "7"
 };
 
-
-var p7 = {
+program_objects[3] = {
     title: "Art Gallery",
     day: "d25",
     start: "9",
     end: "7"
 };
 
-
-var p9 = {
+program_objects[4] = {
     title: "Art Gallery",
     day: "d26",
     start: "9",
     end: "7"
 };
+
+function set_time(pid){
+   //mark when the event starts
+   $(".day."+program_objects[pid].day+" .hour.h"+program_objects[pid].start+" .time").addClass('blocked');
+   //mark when the event ends
+   $(".day."+program_objects[pid].day+" .hour.h"+program_objects[pid].end+" .time").addClass('blocked');
+   //loop through and mark the hours between start and end
+   for(var c = program_objects[pid].start; c < program_objects[pid].end; c++){
+       $(".day."+program_objects[pid].day+" .hour.h"+c+" .time").addClass('blocked');
+   }
+   //add title
+   $(".day."+program_objects[pid].day+" .hour.h"+program_objects[pid].start+" .time")
+       .append('<span class="event-title"> '+program_objects[pid].title+' </span>');
+}
+
+function turn_proid_to_pid(proid){
+    //returns pid
+    //proid is a string set in the html
+    //pid is a number which can be used as an array reference
+    var pid;
+    switch(proid){
+        case 'p1': 
+            pid = 0;
+            break;
+        case 'p3': 
+            pid = 1;
+            break;
+        case 'p5': 
+            pid = 2;
+            break;
+        case 'p7': 
+            pid = 3;
+            break;
+        case 'p9': 
+            pid = 4;
+            break;
+        case 'p11': 
+            pid = 5;
+            break;
+        case 'p13': 
+            pid = 6;
+            break;
+        case 'p15': 
+            pid = 7;
+            break;
+    }
+    return pid;
+}
