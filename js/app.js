389d9651f0d0ef7ad63c3bef4939cb5b4b3fd33a
Ramsey <rdarling42@gmail.com>
2015-10-29 11:08:10 -0400

    working prototype

--- js/app.js
+++ js/app.js
@@ -11,13 +11,37 @@ $(document).ready(function(){
         $('.page').fadeOut();
         $('.page.upcoming-programs').fadeIn();
     });
-    $('.button.tiny').click(function(){
-        $(this).toggleClass('success');
-        //here we mark the schedule as having been updated
+
+    $('.attend').click(function(){
+        //the user wants to attend this event.
+        //first click, we grab the pid
         var pid = $(this).attr('data-proid');
-        set_time(pid);
+        //and call the function to mark the schedule
+        attend(this,pid);
     });
 });
+
+function leave(e,pid){
+    $(e).off('click');//end this click event
+    remove_time(pid);//take the event off the schedule
+    $(e).removeClass('success');//change the color of the button
+    $(e).on('click',function(){
+        //bind the button to another click event, so the same button
+        //can be used to add the event to the users schedule
+        attend(e,pid); 
+    });
+}
+
+function attend(e,pid){
+    $(e).off('click');//end this click event
+    set_time(pid);//put the event on the schedule
+    $(e).addClass('success');//change the color of the button
+    $(e).on('click',function(){
+        //bind the button to another click event, so the same button
+        //can be used to cancel going to the event
+        leave(e,pid); 
+    });
+}
 
 
 function set_time(pid){
@@ -34,20 +58,33 @@ function set_time(pid){
        .append('<span class="event-title"> '+program_objects[pid].title+' </span>');
 }
 
+function remove_time(pid){
+   //remove when the event starts
+   $(".day."+program_objects[pid].day+" .hour.h"+program_objects[pid].start+" .time").removeClass('blocked');
+   //remove when the event ends
+   $(".day."+program_objects[pid].day+" .hour.h"+program_objects[pid].end+" .time").removeClass('blocked');
+   //loop through and remove the hours between start and end
+   for(var c = Number(program_objects[pid].start); c < Number(program_objects[pid].end); c++){
+       $(".day."+program_objects[pid].day+" .hour.h"+c+" .time").removeClass('blocked');
+   }
+   //remove title
+   $(".day."+program_objects[pid].day+" .hour.h"+program_objects[pid].start+" .time .event-title").remove();
+}
+
 //define program objects
 var program_objects = [];
 
 program_objects[0] = {
     title: "ACM Student Research Competition",
     day: "d27",
-    start: "3",
+    start: "15",
     end: "17"
 };
 
 program_objects[1] = {
     title: "Appy Hour",
     day: "d28",
-    start: "5",
+    start: "17",
     end: "19"
 };
 
@@ -55,35 +92,35 @@ program_objects[2] = {
     title: "Art Gallery",
     day: "d24",
     start: "9",
-    end: "19"
+    end: "17"
 };
 
 program_objects[3] = {
     title: "Art Gallery",
     day: "d25",
     start: "9",
-    end: "19"
+    end: "17"
 };
 
 program_objects[4] = {
     title: "Art Gallery",
     day: "d26",
     start: "9",
-    end: "19"
+    end: "17"
 };
 
 program_objects[5] = {
     title: "Art Gallery",
     day: "d27",
     start: "9",
-    end: "19"
+    end: "17"
 };
 
 program_objects[6] = {
     title: "Art Gallery",
     day: "d28",
     start: "9",
-    end: "19"
+    end: "17"
 };
 
 program_objects[7] = {
@@ -294,91 +331,85 @@ program_objects[39] = {
 };
 program_objects[40] = {
     title: "Production Sessions",
-    day: "d24",
+    day: "d27",
     start: "9",
     end: "17"
 };
 program_objects[41] = {
     title: "Production Sessions",
-    day: "d25",
+    day: "d28",
     start: "9",
     end: "17"
 };
 program_objects[42] = {
-    title: "Production Sessions",
+    title: "Real-Time Live",
     day: "d26",
     start: "9",
     end: "17"
 };
 program_objects[43] = {
-    title: "Production Sessions",
-    day: "d27",
+    title: "Studio",
+    day: "d24",
     start: "9",
     end: "17"
 };
 program_objects[44] = {
-    title: "Production Sessions",
-    day: "d28",
+    title: "Studio",
+    day: "d25",
     start: "9",
     end: "17"
 };
 program_objects[45] = {
-    title: "Real-Time Live",
+    title: "Studio",
     day: "d26",
     start: "9",
     end: "17"
 };
 program_objects[46] = {
     title: "Studio",
-    day: "d24",
+    day: "d27",
     start: "9",
     end: "17"
 };
 program_objects[47] = {
     title: "Studio",
-    day: "d25",
+    day: "d28",
     start: "9",
     end: "17"
 };
 program_objects[48] = {
-    title: "Studio",
-    day: "d26",
+    title: "Technical Papers",
+    day: "d25",
     start: "9",
     end: "17"
 };
 program_objects[49] = {
-    title: "Studio",
-    day: "d27",
-    start: "9",
-    end: "17"
-};
-program_objects[50] = {
-    title: "Studio",
-    day: "d28",
+    title: "Technical Papers",
+    day: "d26",
     start: "9",
     end: "17"
 };
 program_objects[50] = {
     title: "Technical Papers",
-    day: "d25",
+    day: "d27",
     start: "9",
     end: "17"
 };
 program_objects[51] = {
     title: "Technical Papers",
-    day: "d26",
+    day: "d28",
     start: "9",
     end: "17"
 };
 program_objects[52] = {
-    title: "Technical Papers",
-    day: "d27",
+    title: "VR Village",
+    day: "d24",
     start: "9",
     end: "17"
 };
 program_objects[53] = {
-    title: "Technical Papers",
-    day: "d28",
+    title: "VR Village",
+    day: "d27",
     start: "9",
     end: "17"
 };
